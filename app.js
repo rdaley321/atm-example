221bc1179609660950b2dca75dcef20192b7c08e
Jesse Wolgamott <jesse@comalproductions.com>
2017-07-11 11:10:19 -0500

    Move to npm run test and separate test db

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const accountRoutes = require("./routes/account");
 const bodyParser = require("body-parser");
 // Connect and stuff
 mongoose.Promise = require("bluebird");
-mongoose.connect("mongodb://127.0.0.1:27017/atm")
+
+// if an outside force has set the "NODE_ENV", use it. default to "development"
+const nodeEnv = process.env.NODE_ENV || "development";
+const config = require("./config")[nodeEnv]
+// If we are testing, use "mongodb://127.0.0.1:27017/atm_test"
+console.log("We are using config.mongoUrl", config.mongoUrl)
+mongoose.connect(config.mongoUrl)
 
 app.use(bodyParser.json())
 app.use(statusRoutes)
